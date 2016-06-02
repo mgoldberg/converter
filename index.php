3ba1081f8c79b020823375fce3b5902e5eb506bd
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-02 16:03:57 -0400

    started styling and put in images

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
 
-
-
-
-
-
 ?>
 
 
@@ -14,17 +9,30 @@
 
 	<head>
 		<title>Converter</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 	</head>
 
 	<body>
 		
 		<div>
-			<a href = "http://localhost/converter/lengthConverter.php">Length</a><br>
-			<a href = "http://localhost/converter/temperatureConverter.php">temperature</a><br>
-			<a href = "http://localhost/converter/weightConverter.php">Weight</a><br>
-			<a href = "http://localhost/converter/liquidConverter.php">Liquid</a><br>
-			<a href = "http://localhost/converter/areaConverter.php">Area</a><br>
-			<a href = "http://localhost/converter/volumeConverter.php">Volume</a>
+			<p>
+				<a href = "http://localhost/converter/lengthConverter.php">Length</a>
+			</p>
+			<p>	
+				<a href = "http://localhost/converter/temperatureConverter.php">temperature</a>
+			</p>
+			<p>	
+				<a href = "http://localhost/converter/weightConverter.php">Weight</a>
+			</p>
+			<p>	
+				<a href = "http://localhost/converter/liquidConverter.php">Liquid</a>
+			</p>
+			<p>	
+				<a href = "http://localhost/converter/areaConverter.php">Area</a>
+			</p>
+			<p>	
+				<a href = "http://localhost/converter/volumeConverter.php">Volume</a>
+			</p>
 		</div>
 
 	</body>
