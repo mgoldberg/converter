e9d4fcd5676c13b4a1c8dfba3c3f11f7bbdd5f35
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-02 13:53:20 -0400

    added ft and in

--- lengthConverter.php
+++ lengthConverter.php
@@ -29,14 +29,44 @@
 					echo $value . "cm";
 				}elseif($convertTo == "m"){
 					echo $value/100 . "m";
+				}elseif($convertTo == "in"){
+					echo $value*0.39370078740157 . "in";
+				}elseif($convertTo == "ft"){
+					echo $value*0.032808398950131 . "ft";
 				}
 			}elseif($type == "m"){
 				if($convertTo == "cm"){
 					echo $value*100 . "cm";
 				}elseif($convertTo == "m"){
 					echo $value . "m";
+				}elseif($convertTo == "in"){
+					echo $value*39.370078740157 . "in";
+				}elseif($convertTo == "ft"){
+					echo $value*3.2808398950131 . "ft";
+				}
+			}elseif($type == "in"){
+				if($convertTo == "cm"){
+					echo $value*2.54 . "cm";
+				}elseif($convertTo == "m"){
+					echo $value*0.0254 . "m";
+				}elseif($convertTo == "in"){
+					echo $value . "in";
+				}elseif($convertTo == "ft"){
+					echo $value*0.083333333333333 . "ft";
+				}
+			}elseif($type == "ft"){
+				if($convertTo == "cm"){
+					echo $value*30.48 . "cm";
+				}elseif($convertTo == "m"){
+					echo $value*0.3048 . "m";
+				}elseif($convertTo == "in"){
+					echo $value*12 . "in";
+				}elseif($convertTo == "ft"){
+					echo $value . "ft";
 				}
 			}
+		}else{
+			echo "Enter a  valid number";
 		}	
 	}
 
@@ -60,12 +90,16 @@
 						<option value="">--Select a unit--</option>
 						<option value="cm">cm</option>
 						<option value="m">m</option>
+						<option value="in">m</option>
+						<option value="ft">m</option>
 						</select>
 				<b>Convert to:</b> 
 						<select name="convertTo">
 						<option value="">--Select a unit--</option>
 						<option value="cm">cm</option>	
-						<option value="m">m</option>		
+						<option value="m">m</option>	
+						<option value="in">m</option>
+						<option value="ft">m</option>	
 						</select>
 
 				<input id="submit" type="submit" value="Convert">
