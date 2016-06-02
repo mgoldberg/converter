54f7815963cf7d30a98950efc5113ad74e3408f2
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-02 13:30:18 -0400

    set up hub site and selectors for length

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE HTML>
+
+	<head>
+		<title>Converter</title>
+	</head>
+
+	<body>
+		
+		<div>
+			<a href = "http://localhost/converter/lengthConverter.php">Length</a>
+		</div>
+
+	</body>
+
+</html>	
---
+++ lengthConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE HTML>
+
+	<head>
+		<title>Converter</title>
+	</head>
+
+	<body>
+		
+		<div>
+			<b>Value:</b> <input type="text" name="value" value=""/><br />
+			<b>Unit type:</b> 
+					<select name="type">
+					<option value="">--Select a unit--</option>
+					<option value="cm">cm</option>
+					<option value="ft">ft</option>
+					</select>
+			<b>Convert to:</b> 
+					<select name="type">
+					<option value="">--Select a unit--</option>
+					<option value="cm">cm</option>	
+					<option value="ft">ft</option>		
+					</select>
+		</div>
+
+	</body>
+
+</html>	
